016dcd30a454eb478cfe2f1cc451778dfdf84453
AnAramyan <anahitaramyan95@gmail.com>
2023-11-21 15:22:41 +0400

    Change Add Set  Page

--- src/components/addNewSet/AddNewSet.tsx
+++ src/components/addNewSet/AddNewSet.tsx
@@ -17,6 +17,7 @@ const AddNewSet = () => {
     setError,
     control,
     reset,
+    setValue,
   } = useForm<Set>();
   const addFunc = (data: any) => {
     if (data.foods.length < 2) {
@@ -37,7 +38,7 @@ const AddNewSet = () => {
     }
 
     return foods;
-  }, [activeCategory]);
+  }, [activeCategory, foods]);
 
   return (
     <>
@@ -120,16 +121,28 @@ const AddNewSet = () => {
                   className="w-full border-b border-gray-200 rounded-t-lg"
                 >
                   <div className="flex items-center ps-3">
-                    <input
-                      id={`food${el.id}`}
-                      type="checkbox"
-                      value={el.id}
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      {...register("foods", {
-                        required: "Field is required",
-                      })}
+                    <Controller
+                      control={control}
+                      name="foods"
+                      render={({ field: { onBlur, value } }) => (
+                        <input
+                          type={"checkbox"}
+                          onBlur={onBlur}
+                          id={`food${el.id}`}
+                          onChange={(e) => {
+                            value = value || [];
+                            const isChecked = e.target.checked;
+                            let newValue = isChecked
+                              ? [...value, el.id]
+                              : value.filter((id) => id !== el.id);
+                            setValue("foods", newValue);
+                          }}
+                          checked={
+                            Array.isArray(value) && value.includes(el.id)
+                          }
+                        />
+                      )}
                     />
-
                     <label
                       htmlFor={`food${el.id}`}
                       className="w-full py-3 ms-2 text-sm font-medium text-gray-900"
